8b13333c9b23a1eaf6069be82aca7651813e09f0
pmb9308613 <162384210+pmb9308613@users.noreply.github.com>
2025-06-04 21:03:33 -0700

    updated getWeekday function

--- src/script.js
+++ src/script.js
@@ -40,21 +40,15 @@ function updateWeatherInformation(response) {
 }
 
 function getWeekday(num) {
-  if (num === 0) {
-    return "Sunday";
-  } else if (num === 1) {
-    return "Monday";
-  } else if (num === 2) {
-    return "Tuesday";
-  } else if (num === 3) {
-    return "Wednesday";
-  } else if (num === 4) {
-    return "Thursday";
-  } else if (num === 5) {
-    return "Friday";
-  } else if (num === 6) {
-    return "Saturday";
-  }
+  return [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ][num];
 }
 
 function searchCity(city) {
