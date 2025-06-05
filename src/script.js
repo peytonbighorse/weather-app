88b6fa3d0c6148737a29d81536b911c5fbfa3274
pmb9308613 <162384210+pmb9308613@users.noreply.github.com>
2025-06-04 20:47:26 -0700

    Added API data for humidity, wind speed, date, and emoji image

--- src/script.js
+++ src/script.js
@@ -1,10 +1,61 @@
 function updateWeatherInformation(response) {
+  console.log(response.data);
+  //temperature
   let temperature = Math.round(response.data.temperature.current);
-  let newCity = response.data.city;
   let temperatureElement = document.querySelector(".weather-temperature");
+  //humidity
+  let humidity = response.data.temperature.humidity;
+  let humidityElement = document.querySelector(".humidity");
+  //wind
+  let windSpeed = response.data.wind.speed;
+  let windSpeedElement = document.querySelector(".wind-speed");
+  //city
+  let newCity = response.data.city;
   let cityElement = document.querySelector(".weather-data-city");
+  // condtion
+  let weatherCondition = response.data.condition.description;
+  let weatherConditionElement = document.querySelector(".weather-condition");
+  //condition emoji
+  let conditionEmojiURL = response.data.condition.icon_url;
+  let emojiElement = document.querySelector(".weather-emoji");
+
+  //time and date
+  let dateElement = document.querySelector(".day-and-time");
+  let timestamp = response.data.time;
+  let fullDate = new Date(timestamp * 1000);
+  let currentWeekDay = getWeekday(fullDate.getDay());
+  let hours = fullDate.getHours();
+  let minutes = fullDate.getMinutes();
+  let currentDate = `${currentWeekDay} ${hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }`;
+
+  //changes
   temperatureElement.innerHTML = temperature;
+  humidityElement.innerHTML = humidity;
+  windSpeedElement.innerHTML = windSpeed;
   cityElement.innerHTML = newCity;
+  dateElement.innerHTML = currentDate;
+  weatherConditionElement.innerHTML = weatherCondition;
+  emojiElement.innerHTML = `<img src="${conditionEmojiURL}" alt="">`;
+}
+
+function getWeekday(num) {
+  if (num === 0) {
+    return "Sunday";
+  } else if (num === 1) {
+    return "Monday";
+  } else if (num === 2) {
+    return "Tuesday";
+  } else if (num === 3) {
+    return "Wednesday";
+  } else if (num === 4) {
+    return "Thursday";
+  } else if (num === 5) {
+    return "Friday";
+  } else if (num === 6) {
+    return "Saturday";
+  }
 }
 
 function searchCity(city) {
